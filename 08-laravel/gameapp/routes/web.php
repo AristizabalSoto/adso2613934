bf46f67e5e29db24402c2b3237ffb110ca601c4a
AristizabalSoto <martinrobinsofficial@gmail.com>
2024-06-20 21:08:13 -0500

    creacion de vistas

--- 08-laravel/gameapp/routes/web.php
+++ 08-laravel/gameapp/routes/web.php
@@ -1,7 +1,54 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
+use Illuminate\Support\Carbon;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// ----------------------------------------------------------------------------------------------------------------
+
+Route::get('/games/list', function () {
+    $games = App\Models\Game::all();
+    dd($games); 
+});
+
+// -------------------------------------------------------------------------------------------------------------
+
+Route::get('/consulta/{id}', function ($id) {
+    $user = User::findOrFail($id);
+    $fullName = $user->fullname;
+    $ageInYears = Carbon::parse($user->birthdate)->age;
+    $createdAt = $user->created_at->diffForHumans();
+
+    return "User: $fullName | Age: $ageInYears years | Created: $createdAt";
+});
+
+// ---------------------------------------------------------------------------------------------------------
+
+Route::get('/consulta', function () {
+    $users = User::take(20)->get();
+
+    $output = [];
+
+    foreach ($users as $user) {
+        $fullName = $user->fullname;
+        $ageInYears = Carbon::parse($user->birthdate)->age;
+        $createdAt = $user->created_at->diffForHumans();
+
+        $output[] = "User: $fullName | Age: $ageInYears years | Created: $createdAt";
+    }
+
+    return implode('<br>', $output);
+});
+
+// --------------------------------------------------------------------------------------------------------------
+
+Route::get('/games', function () {
+    $games = App\Models\Game::all();
+    return view('listgames')->with('games', $games);
+});
+
+
