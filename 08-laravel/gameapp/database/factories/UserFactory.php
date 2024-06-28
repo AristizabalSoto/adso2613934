fb932ec431560f452d959618d90b42f9f220a7db
AristizabalSoto <martinrobinsofficial@gmail.com>
2024-06-27 20:40:19 -0500

    actualizacion

--- 08-laravel/gameapp/database/factories/UserFactory.php
+++ 08-laravel/gameapp/database/factories/UserFactory.php
@@ -2,70 +2,51 @@
 
 namespace Database\Factories;
 
-use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
 class UserFactory extends Factory
 {
     /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
-    protected $model = User::class;
-
-    /**
      * The current password being used by the factory.
-     *
-     * @var string|null
      */
     protected static ?string $password;
 
     /**
      * Define the model's default state.
      *
-     * @return array
+     * @return array<string, mixed>
      */
-    public function definition()
-    {
-        $gender = $this->faker->randomElement(['female', 'male']);
-
-        $firstName = ($gender == 'male') ? $this->faker->firstNameMale() : $this->faker->firstNameFemale();
-
-        // Generate a unique filename for the photo
-        $photoName = Str::random(10) . '.jpg'; // Example: randomname.jpg
-
-        // Generate and store the image in storage/app/public/images
-        $this->faker->image(storage_path('app/public/images'), 200, 200, 'people', false, true, 'Faker');
+    public function definition(): array
+    {   
 
         return [
-            'document' => $this->faker->randomNumber(9, true),
-            'fullname' => $firstName . ' ' . $this->faker->lastName(),
-            'gender' => $gender,
-            'birthdate' => $this->faker->dateTimeBetween('-50 years', '-20 years')->format('Y-m-d'),
-            'photo' => 'images/' . $photoName,
-            'phone' => $this->faker->phoneNumber(),
-            'email' => $this->faker->unique()->safeEmail(),
+            'document'          =>fake()->isbn13(),
+            'gender'            => $gender=fake()->randomElement(['Male','Female']),
+            'fullname'          => fake()->name($gender)." ".fake()->lastName(),
+            'birthdate'         => fake()->dateTimeBetween('-30 years','2004-01-01'),
+            'phone'             => fake()->phoneNumber(),
+            'photo'             => substr(fake()->image($dir = './public/images', $width = 50, $height = 50),13),
+            'email'             => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('12345'),
-            'remember_token' => Str::random(10),
+            'password'          => static::$password ??= Hash::make('password'),
+            'remember_token'    => Str::random(10),
         ];
     }
 
     /**
      * Indicate that the model's email address should be unverified.
-     *
-     * @return \Illuminate\Database\Eloquent\Factories\Factory
      */
-    public function unverified()
+    public function unverified(): static
     {
-        return $this->state(function (array $attributes) {
-            return [
-                'email_verified_at' => null,
-            ];
-        });
+        return $this->state(fn (array $attributes) => [
+            'email_verified_at' => null,
+        ]);
     }
 }
 
